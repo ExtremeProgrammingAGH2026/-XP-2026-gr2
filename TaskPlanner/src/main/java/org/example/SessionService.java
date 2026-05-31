1da5c1e29a6ad0e4b20a4b0e46859161ca6f82a6
Maciej Mezyk <mcj.mezyk@gmail.com>
2026-05-31 16:56:09 +0200

    Init RED

---
+++ TaskPlanner/src/main/java/org/example/SessionService.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class SessionService {
+
+    public void login(User user) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public User getCurrentUser() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean isLoggedIn() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void logout() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
