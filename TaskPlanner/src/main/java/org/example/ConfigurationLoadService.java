1b54b85c2253c6796d5a1648033430ff80d893ef
matrzask <maciejtrzask@student.agh.edu.pl>
2026-06-14 14:21:46 +0200

    RED: config load tests

---
+++ TaskPlanner/src/main/java/org/example/ConfigurationLoadService.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jshell.spi.ExecutionControl;
+
+public class ConfigurationLoadService {
+
+    public ConfigurationLoadService() {
+    }
+
+    public AppConfiguration loadConfiguration(String filePath) throws IOException {
+        return null;
+    }
+}
